77df70845456e5b23d806756c8c348cad34d3102
Omar <omar.cpp@hotmail.com>
2021-10-11 22:33:15 -0400

    add comments and update README

--- project/tools/out_of_vocab.py
+++ project/tools/out_of_vocab.py
@@ -1,12 +1,15 @@
 import methods
 
-files_words = {}
+files_words = {} # dictionary to store the words of files to save time an not look them up later
 methods.mkdir("./scores/")
 output_file = "./scores/out_of_vocab.txt"
 output = ("parsed file \t# of words \t# of unique words "
           "\ttraining set \t# of words \t# of unique words"
           "\t# of out-of-vocab \t# of unique out-of-vocab\n")
 
+# a function that takes a conllu file name as an input
+# it returns two lists, the first one contains all the words (tokens) of the file
+# the second one contains the unique words of the file
 def get_words_uniq(file):
     words_list = []
     with open(file, "r") as f:
@@ -22,18 +25,21 @@ def get_words_uniq(file):
     
     return [words_list, u_words_list]
 
-def out_of_voc(file1, file2):   #file2 is the one used for the training model, file1 is the parsed one
+# a function that calculates the out-of-vocab and unique out-of-vocab and adds them to the output
+# file1 is the parsed file, file2 is the one used for the training model
+def out_of_voc(file1, file2):   
     global files_words, output
     
+    # if the list of words is not found in the dictionary, use the above function to get it
     if not file1 in files_words.keys():
         files_words[file1] = get_words_uniq(file1)
     if not file2 in files_words.keys():
         files_words[file2] = get_words_uniq(file2)
-
-    '''uniq_count = 0
-    all_words =  list(set(files_words[file1][1] + files_words[file2][1]))
-    uniq_count = len(all_words) - len(files_words[file2][1])'''
-            
+    
+    # this part goes through the list of words for the parsed file
+    # and checks if it is available in the training file
+    # keep in mind: files_words[file1][0] is the number of words in file1
+    # files_words[file1][1] is the number of unique words in file1
     count = 0
     in_vocab = []
     out_vocab = []
@@ -59,6 +65,7 @@ def out_of_voc(file1, file2):   #file2 is the one used for the training model, f
 
 config = methods.json_load("./config.json")
 
+# go through all the parsing sets and run the above function on their parsing-training pairs
 for p_set in config['parse_sets']:
     for pair in p_set['model_input_pairs']:
         file1 = p_set['data_path'] + pair[1]
@@ -71,5 +78,6 @@ for p_set in config['parse_sets']:
         print(f"running for files {file1} and {file2}")
         out_of_voc(file1, file2)
 
+#write the output to a file
 with open(output_file, "w") as f:
     f.write(output)
